da7ab830d2245c9f4fb61532544673ed7e056ef5
Leonardo Valencia <leov@gammawizard.com>
2026-05-12 22:39:25 -0400

    Mas multi tasking

--- async/Program.fs
+++ async/Program.fs
@@ -7,6 +7,15 @@
 open System
 open System.Threading
 
+
+let mostrarMensaje x y color (msg:string) =
+    Console.SetCursorPosition(x,y)
+    Console.ForegroundColor <- color
+    msg |> Console.Write
+
+let morstrarMensajeDerecha y color (msg:string) =
+    let start = Console.BufferWidth-msg.Length
+    mostrarMensaje start y color msg
 let timerUno() =
     async {
         do! Async.Sleep 5000
@@ -43,5 +52,33 @@ let evaluar() =
 // un reloj cada segundo, algo como timer=0, timer=1, ...
 //
 
-evaluar() |> Async.RunSynchronously
+let rec reloj x =
+    async {
+        morstrarMensajeDerecha 0 ConsoleColor.Yellow $"Timer={x}"
+        do! Async.Sleep 1000
+        return! reloj (x+1)
+    }
+
+let leerTeclado() = 
+    async {
+        while true do
+            if Console.KeyAvailable then 
+                let k = Console.ReadKey true
+                mostrarMensaje 0 10 ConsoleColor.Cyan $"Tecla presionada: {k.Key}"
+            //do! Async.Sleep 10
+    }
+
+Console.Clear()
+Console.CursorVisible <- false
+
+//
+// Es importante que toda la Salida en pantalla del programa ocurra en el
+// mismo thread. 
+//
+// A ese hilo se le llama el UI thread
+//
+leerTeclado() |> Async.Start // Arranca una computacion en el Thread pool
+evaluar() |> Async.StartImmediate // Arranca una computacion en el thread actual
+
+reloj 0 |> Async.RunSynchronously
 
